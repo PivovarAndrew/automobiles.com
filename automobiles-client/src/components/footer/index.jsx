e68094796855632ff1f12962456739234945d100
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2024-03-13 03:19:30 +0300

    Add main footer information

--- automobiles-client/src/components/footer/index.jsx
+++ automobiles-client/src/components/footer/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './footer.module.scss';
 import logo from 'logo.svg';
+import { FaYoutube, FaTelegram, FaTiktok, FaInstagram } from "react-icons/fa6";
 
 const Footer = () => {
     return (
@@ -10,7 +11,45 @@ const Footer = () => {
                     <img src={logo}></img>
                     <div className={styles.logoTitle}>automobiles.com</div>
                 </section>
-                
+                <section>
+                    <h3>Наши соц сети</h3>
+                    <ul >
+                        <li>
+                            <FaYoutube className={styles.youtube} />
+                            <label>Youtube</label>
+                        </li>
+                        <li>
+                            <FaTelegram className={styles.telegram} />
+                            <label>Telegram</label>
+                        </li>
+                        <li>
+                            <FaTiktok />
+                            <label>Tik Tok</label>
+                        </li>
+                        <li>
+                            <FaInstagram />
+                            <label>Instagram</label>
+                        </li>
+                    </ul>
+                </section>
+                <section>
+                    <h3>Контакты</h3>
+                    <ul>
+                        <li>
+                            <a href="automobiles@gmail.com">
+                                Почтовый адрес: automobiles@gmail.com
+                            </a>
+                        </li>
+                        <li>
+                            <a href="tel:+375441001010">Телефон: +375 (44) 100 10 10</a>
+                        </li>
+                        <li>
+                            <a href="automobiles@gmail.com">
+                                Физический адрес: г. Москва, проспект Пушкина 15, офис 451
+                            </a>
+                        </li>
+                    </ul>
+                </section>
             </div>
             Copyright @ Automobiles.com &emsp; | &emsp;  All rights reserved 2024
         </footer>
