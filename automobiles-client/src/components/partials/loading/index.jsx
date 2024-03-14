e183d2cef2198869e15026dadf77bf9472edcace
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2024-03-14 18:03:16 +0300

    Create Loading component

---
+++ automobiles-client/src/components/partials/loading/index.jsx
@@ -0,0 +1,12 @@
+import { ImSpinner2 } from 'react-icons/im';
+
+/**
+ * Decorative component that represents loading.
+ *
+ * @return { ReactElement } The loading.
+ */
+const Loading = () => (
+    <ImSpinner2 />
+)
+
+export default Loading;
