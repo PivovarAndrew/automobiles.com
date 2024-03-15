c84c68bd408c64c44f45c24014cd944b73906d3d
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2024-03-15 22:33:36 +0300

    Add suspense fallback for app

--- automobiles-client/src/App.jsx
+++ automobiles-client/src/App.jsx
@@ -3,18 +3,22 @@ import Header from 'components/header';
 import Footer from 'components/footer';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from 'components/home';
+import { Suspense } from 'react';
+import Loading from 'components/partials/loading';
 
 function App() {
   return (
     <div className="App container">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <Suspense fallback={<Loading />}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </Suspense>
     </div>
   );
 }
