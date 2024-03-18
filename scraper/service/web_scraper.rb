c4804c463ddc5f32e225f8d97e8e39a3b0e41f84
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2024-03-19 02:40:21 +0300

    Add methods of text and images url scrape

--- scraper/service/web_scraper.rb
+++ scraper/service/web_scraper.rb
@@ -3,11 +3,23 @@
 
 require "net/http"
 require "nokogiri"
+require "nokogiri-styles"
+require "./service/url_parser"
 
 # Class responsible for scrap and import required information from the web site
 class WebScraper
   def initialize(web_site_info)
     @web_site_info = web_site_info
+  end
+
+  private
+
+  def scrape_text(css_class)
+    @document.css(css_class).map { |element| element.text }
+  end
+
+  def scrape_images_url(css_class)
+    @document.css(css_class).map { |element| URLParser.parse(element.styles["background-image"]) }
   end
 
   def prepare_document(url)
